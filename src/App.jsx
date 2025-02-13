56d4748baebeb0c8ba21d035adfc5cd75d76d76f
youcef23061983 <youcef.belcourtois@gmail.com>
2025-02-13 16:04:45 +0100

    3new

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Meta from "./components/Meta";
 const App = () => {
   return (
     <div>
-      <Meta
+      {/* <Meta
         post={{
           title: "new meta method",
           description: "wait and see",
@@ -16,7 +16,36 @@ const App = () => {
           keywords: " keywords of meta react19",
           authorContent: "author content of react19 meta",
         }}
-      />
+      /> */}
+      <article>
+        <h1>new 19</h1>
+        <title>new 19</title>
+        <meta name="description" content="new 19" />
+        <meta property="og:title" content="new 19" />
+        <meta property="og:description" content="new 19 description" />
+        <meta
+          property="og:image"
+          content="https://codeevolution.vercel.app/1.jpg"
+        />
+        <meta property="og:url" content="https://codeevolution.vercel.app" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" ccontent="new 19" />
+        <meta name="twitter:description" content="new 19 description" />
+        <meta
+          property="twitter:domain"
+          content="https://codeevolution.vercel.app"
+        />
+
+        <meta
+          name="twitter:image"
+          content="https://codeevolution.vercel.app/1.jpg"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content="new 19 keywords" />
+        <link rel="author" content="new 19 author" />
+      </article>
 
       <h1>Hello, world! hi</h1>
     </div>
